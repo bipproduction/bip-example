41751b7c5540aaa3d4ba04e5411ab3d4e0c12756
bipproduction <bip.production.js@gmail.com>
2023-10-17 14:44:43 +0800

    title: auto des: auto note:auto

--- src/app/router-search-param/page.tsx
+++ src/app/router-search-param/page.tsx
@@ -1,12 +1,18 @@
-import { Paper, Stack, Title, UnstyledButton } from "@mantine/core"
+import { Flex, Paper, Stack, Title, UnstyledButton } from "@mantine/core"
 import Link from "next/link"
 import WidgetDataParam from "./_widget/data_param"
+import { IconArrowBack, IconArrowLeft } from "@tabler/icons-react"
 
 export default function Page({ searchParams }: { searchParams: { prov: string } }) {
     const list_param = ["satu", "dua", "tiga", "empat"]
     return <>
         <Stack p={"md"}>
-            <Title>Route With Param</Title>
+            <Flex p={"xs"} gap={"md"} align={"center"}>
+                <Link href={'/'}>
+                    <IconArrowLeft size={36} />
+                </Link>
+                <Title>Route With Param</Title>
+            </Flex>
             {list_param.map((v, k) => <Link key={k} href={`/router-search-param?prov=${v}`} >
                 <UnstyledButton>
                     Coba {v}
